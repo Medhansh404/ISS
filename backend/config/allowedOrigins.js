3a8bd9a8281c6376ad985e46e3192eb4290024bf
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-12-14 20:42:49 +0530

    env variables

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://iss-medhansh404s-projects.vercel.app',
+    'https://iss-g8fo.onrender.com',
     'http://127.0.0.1:5500',
     'http://localhost:3000'
 ];
